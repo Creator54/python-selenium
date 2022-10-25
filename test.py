df52eb901f9841765d094efadafdfe42bea8a599
creator54 <hi.creator54@gmail.com>
2022-10-26 02:01:37 +0530

    update example

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Firefox()
-browser.get('http://selenium.dev/')
+browser.get('http://google.com')
+
+search = browser.find_element(By.NAME, 'q')
+search.send_keys('seleniumhq' + Keys.RETURN)
